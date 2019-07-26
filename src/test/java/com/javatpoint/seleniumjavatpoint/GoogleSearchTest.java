66d5df98928f7705af541e953eafdaea388193c1
TonyYuta <tonykagaya@gmail.com>
2019-07-26 16:00:14 -0700

    add tc title google main page

--- src/test/java/com/javatpoint/seleniumjavatpoint/GoogleSearchTest.java
+++ src/test/java/com/javatpoint/seleniumjavatpoint/GoogleSearchTest.java
@@ -5,15 +5,28 @@ import org.testng.annotations.Test;
 
 public class GoogleSearchTest extends BaseTest{
 
+    @Test(description = "Title of Google main page",
+        enabled = true,
+        groups = {"bat", "sanity", "regression", "Google"},
+        priority = 0)
+    public void testGetGoogleMainPageTitle(){
+        String expected = "Google";
+        Assert.assertEquals(googleSearch.currentPageTitle(), expected,
+                "current Page Title doesn't match to Google main page");
+    }
+
     @Test(description = "Navigate to About Page",
         enabled = true,
         groups = {"bat", "regression", "all", "HomePage"},
+        dependsOnMethods = "testGetGoogleMainPageTitle",
+        dependsOnGroups = {"sanity"},
         priority = 0
     )
     public void testNavigateToAboutPage(){
         String expected = "About | Google";
         googleSearch.navigateToAboutPage();
-        Assert.assertEquals(aboutPage.currentPageTitle(), expected, "current Page Title doesn't match to About page");
+        Assert.assertEquals(aboutPage.currentPageTitle(), expected,
+                "current Page Title doesn't match to About page");
     }
 
 }
