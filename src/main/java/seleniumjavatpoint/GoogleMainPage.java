595ccb2e35a0789ceecd5fc8b9bec489a054f4d4
TonyYuta <tonykagaya@gmail.com>
2019-07-27 19:26:50 -0700

    fixed search test

--- src/main/java/seleniumjavatpoint/GoogleMainPage.java
+++ src/main/java/seleniumjavatpoint/GoogleMainPage.java
@@ -1,10 +1,15 @@
 package seleniumjavatpoint;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
+
+
+import static org.openqa.selenium.WebElement.*;
 
 public class GoogleMainPage extends BasePage{
 
@@ -12,14 +17,29 @@ public class GoogleMainPage extends BasePage{
         super(driver);
     }
 
-    WebDriverWait wait = new WebDriverWait(driver, 20);
+    WebDriverWait wait = new WebDriverWait(driver, 5);
 
-    private static String googleUrl = "https://google.com/";
+  //  private static String googleUrl = "https://google.com/";
 
-    // cssSelector
-    private String homePageTab = "#orb-nav-links > ul > li.orb-nav-homedotcom.orb-w > a";
-
+    //cssSelector
+    private String googleMainPageUrl = "https://google.com/";
+    //private By searchBox = By.cssSelector(".RNNXgb>div>div");
+    private By searchBox = By.cssSelector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input");
     private By aboutLink = By.cssSelector("#hptl>a:nth-child(1)");
+    private By googleSearhBtn = By.cssSelector("div.FPdoLc.VlcLAe>center>input.gNO89b[value='Google Search']");
+
+    //id
+    private By resultStatsText = By.id("resultStats");
+
+    public GoogleMainPage navigateToMainPage(){
+        driver.navigate().to(googleMainPageUrl);
+        return new GoogleMainPage(driver);
+    }
+
+
+/*    public void navigateToMainPage() {
+        driver.navigate().to(googleMainPageUrl);
+    }*/
 
 
     public GoogleAboutPage navigateToAboutPage(){
@@ -28,6 +48,29 @@ public class GoogleMainPage extends BasePage{
 
         aboutLinkElement.click();
         return new GoogleAboutPage(driver);
+    }
+
+    /**
+     * @param text
+     * @return String of Search Statistics
+     */
+    public String searchText(String text){
+        WebElement searchBoxElement = wait.until(ExpectedConditions.presenceOfElementLocated(searchBox));
+        System.out.println("=======================debug1==================================");
+        searchBoxElement.sendKeys(text);
+        System.out.println("=======================debug2==================================");
+
+        WebElement googleSearhBtnElement = wait.until(ExpectedConditions.elementToBeClickable(googleSearhBtn));
+        System.out.println("=======================debug3==================================");
+
+        //googleSearhBtnElement.click();
+        googleSearhBtnElement.sendKeys(Keys.RETURN);
+        System.out.println("=======================debug4==================================");
+
+        WebElement resultStatsTextElement = wait.until(ExpectedConditions.presenceOfElementLocated(resultStatsText));
+        System.out.println("=======================debug5==================================");
+
+        return resultStatsTextElement.getText();
     }
 
 
--- src/test/java/com/javatpoint/seleniumjavatpoint/GoogleMainPageTest.java
+++ src/test/java/com/javatpoint/seleniumjavatpoint/GoogleMainPageTest.java
@@ -1,32 +1,74 @@
 package com.javatpoint.seleniumjavatpoint;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GoogleMainPageTest extends BaseTest{
 
+    @BeforeMethod(alwaysRun=true)
+    public void beforeMethodGoogleMainPage(){
+        //public void navigateToMainPage(){
+    }
+
+
+    @AfterMethod(enabled=true, alwaysRun=true)
+    public void quitDriver(){
+        //driver.close();
+        //driver.quit();
+    }
+
+
     @Test(description = "Title of Google Main page",
         enabled = true,
         groups = {"bat", "sanity", "regression", "Google"},
-        priority = 0)
+        priority = 1)
     public void testGetGoogleMainPageTitle(){
         String expected = "Google";
         Assert.assertEquals(googleMainPage.currentPageTitle(), expected,
                 "current Page Title doesn't match to Google main page");
     }
 
+
     @Test(description = "Navigate to Google About Page",
         enabled = true,
         groups = {"bat", "regression", "all", "HomePage"},
         dependsOnMethods = "testGetGoogleMainPageTitle",
         dependsOnGroups = {"sanity"},
-        priority = 0
-    )
+        priority = 1)
     public void testNavigateToGoogleAboutPage(){
         String expected = "About | Google";
+        googleMainPage.navigateToMainPage();
         googleMainPage.navigateToAboutPage();
         Assert.assertEquals(googleAboutPage.currentPageTitle(), expected,
                 "current Page Title doesn't match to About page");
     }
+
+
+    @Test(description = "Search text",
+            enabled = true,
+            groups = {"bat", "search"},
+            dependsOnGroups = "sanity",
+            dependsOnMethods="testGetGoogleMainPageTitle",
+            priority = 0)
+    public void testSearchText(){
+        String searchText = "javatpoint tutorials";
+        String expected = "About";
+        googleMainPage.navigateToMainPage();
+        Assert.assertTrue(googleMainPage.searchText(searchText).contains(expected),"mismatch ir Search Results");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
